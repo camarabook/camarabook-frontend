f8b414f24623c4074f7b49796957566602aedcd6
Duke <duke@riseup.net>
2013-12-16 10:39:19 -0200

    fixed nofollow link

--- assets/javascripts/app.js
+++ assets/javascripts/app.js
@@ -13,13 +13,14 @@ $(document).ready(function(){
   window.router = new Router();
   Backbone.history.start({pushState: true});
   document.createElement('video');document.createElement('audio');
-  $("a:not(.nolink)").live('click', function(event){
+  $("a:not(.nofollow)").live('click', function(event){
     href = $(event.currentTarget).attr("href");
     router.navigate(href, {trigger: true});
     loading = new Loading();
     event.preventDefault();
   });
 });
+
 Number.prototype.formatMoney = function(c, d, t){
   var n = this, c = isNaN(c = Math.abs(c)) ? 2 : c, d = d == undefined ? "," : d, t = t == undefined ? "." : t, s = n < 0 ? "-" : "", i = parseInt(n = Math.abs(+n || 0).toFixed(c)) + "", j = (j = i.length) > 3 ? j % 3 : 0;
   return s + (j ? i.substr(0, j) + t : "") + i.substr(j).replace(/(\d{3})(?=\d)/g, "$1" + t) + (c ? d + Math.abs(n - i).toFixed(c).slice(2) : "");
