3e82e626670b02cd87b4475c892704e1d3184475
Duke <duke@riseup.net>
2014-08-06 06:05:14 -0300

    added deputado about

--- app/models/deputado.js
+++ app/models/deputado.js
@@ -1,16 +1,18 @@
 import DS from 'ember-data';
+var attr = DS.attr;
 
 export default DS.Model.extend({
-  cadastro_id: DS.attr(),
-  parlamentar_id: DS.attr(),
-  nome: DS.attr(),
-  nome_parlamentar: DS.attr(),
-  sexo: DS.attr(),
-  uf: DS.attr(),
-  gabinete: DS.attr(),
-  anexo: DS.attr(),
-  fone: DS.attr(),
-  email: DS.attr(),
-  uri: DS.attr(),
-  image_url: DS.attr(),
+  cadastro_id: attr('number'),
+  parlamentar_id: attr('number'),
+  nome: attr('string'),
+  nome_parlamentar: attr('string'),
+  sexo: attr('string'),
+  uf: attr('string'),
+  gabinete: attr('number'),
+  anexo: attr('number'),
+  fone: attr('string'),
+  email: attr('string'),
+  uri: attr('string'),
+  image_url: attr('string'),
+  sobre: attr('about'),
 });
