87897df96ad7ab7d9353996172e4f1ae665c1a93
Duke <duke@riseup.net>
2014-08-15 22:23:31 -0300

    using adapter host by config

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,6 +1,6 @@
 import DS from 'ember-data';
 
 export default DS.RESTAdapter.extend({
-  host: 'http://127.0.0.1:5001',
+  host: window.CamarabookENV.API_URL,
   namespace: "v1"
 });
--- config/environment.js
+++ config/environment.js
@@ -7,31 +7,26 @@ module.exports = function(environment) {
     locationType: 'auto',
     EmberENV: {
       FEATURES: {
-        // Here you can enable experimental features on an ember canary build
-        // e.g. 'with-controller': true
       }
     },
 
     APP: {
-      // Here you can pass flags/options to your application instance
-      // when it is created
     }
   };
 
   if (environment === 'development') {
-    // ENV.APP.LOG_RESOLVER = true;
     ENV.APP.LOG_ACTIVE_GENERATION = true;
     ENV.APP.LOG_TRANSITIONS = true;
     ENV.APP.LOG_TRANSITIONS_INTERNAL = true;
     ENV.APP.LOG_VIEW_LOOKUPS = true;
+    ENV.API_URL = 'http://127.0.0.1:5001';
   }
 
   if (environment === 'test') {
-
   }
 
   if (environment === 'production') {
-
+    ENV.API_URL = 'http://camarabook-api.herokuapp.com';
   }
 
   return ENV;
