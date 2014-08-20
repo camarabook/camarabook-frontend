027e78567c65cc3eed6d43f190dffd1e7951cf8c
Duke <duke@riseup.net>
2014-08-20 05:46:57 -0300

    removed unused files

--- tests/unit/views/parliamentarian/about-test.js
+++
@@ -1,9 +0,0 @@
-import { test, moduleFor } from 'ember-qunit';
-
-moduleFor('view:parliamentarian/about', 'ParliamentarianAboutView');
-
-// Replace this with your real tests.
-test('it exists', function() {
-  var view = this.subject();
-  ok(view);
-});
